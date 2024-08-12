ac5e5d1232af4bac7b1ec514e51ac88c1ec71e66
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-12 08:04:14 +0100

    fix: src/presentation/pages/sidebar-pages/lab-virtual.tsx. Criação de um botão em src/presentation/pages/sidebar-pages/lab-virtual.tsx; Estilização de CSS em src/presentation/pages/sidebar-pages/lab-virtual.tsx; Modificação de código em src/presentation/pages/sidebar-pages/lab-virtual.tsx; Alteração de texto em src/presentation/pages/sidebar-pages/lab-virtual.tsx; Mudança de texto em um elemento HTML em src/presentation/pages/sidebar-pages/lab-virtual.tsx; Alteração em funções de manipulação de eventos em src/presentation/pages/sidebar-pages/lab-virtual.tsx; Alteração de conteúdo em array ou objeto em src/presentation/pages/sidebar-pages/lab-virtual.tsx

--- src/presentation/pages/sidebar-pages/lab-virtual.tsx
+++ src/presentation/pages/sidebar-pages/lab-virtual.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import { useState } from 'react';
 import { Navbar } from '../../components/dashboard-components/navbar/navbar';
-import { Sidebar } from '../../components/dashboard-components/sidebar/sidebar'; 
+import { Sidebar } from '../../components/dashboard-components/sidebar/sidebar';
 import { useAuth } from '@/context/auth-context';
 import { ROUTE_TRAININGS } from '@/utils/sidebar-utils';
 import { ListLabs } from '@/presentation/components/dashboard-components/list-labs/list-labs';
 import { FaCartPlus } from 'react-icons/fa6';
+import { AddLabForm } from '../admin-pages/add-labs';
 
 export const VirtualLabs: React.FC = () => {
 
@@ -27,8 +28,8 @@ export const VirtualLabs: React.FC = () => {
       <div className={`min-h-screen p-4 sm:pt-20 ${isOpen ? 'sm:ml-[10rem]' : 'sm:ml-[4rem]'} transition-all duration-300 bg-gray-100 dark:bg-gray-900`}>
 
         <br />
-        <div className="flex dark:bg-slate-100/20 dark:text-white justify-between w-full px-5 py-6 bg-white rounded-lg shadow">
-          <h2 className="sm:text-3xl text-2xl font-bold hacker">Labs Virtuais</h2>
+        <div className="flex justify-between w-full px-5 py-6 bg-white rounded-lg shadow dark:bg-slate-100/20 dark:text-white">
+          <h2 className="text-2xl font-bold sm:text-3xl hacker">Labs Virtuais</h2>
 
           {
             /*
@@ -36,20 +37,19 @@ export const VirtualLabs: React.FC = () => {
             text='Comprar treinamento'
             onClick={() => window.location.href = ROUTE_TRAININGS}
             color='primary'
-            className='click hidden sm:block'
+            className='hidden click sm:block'
           />
-
             */
           }
 
           <button className='click sm:hidden' onClick={() => window.location.href = ROUTE_TRAININGS}>
-            <FaCartPlus className='my-auto dark:text-white text-black text-3xl sm:text-4xl' />
+            <FaCartPlus className='my-auto text-3xl text-black dark:text-white sm:text-4xl' />
           </button>
         </div>
         <br />
 
 
-        <ListLabs />
+        <AddLabForm />
 
 
       </div>
