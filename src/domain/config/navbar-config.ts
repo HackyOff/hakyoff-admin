ae1e2c76ecfcfd8119afd7b4a0d3888d54040a39
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-12 09:03:24 +0100

    fix: src/domain/config/navbar-config.ts. Modificação de código em src/domain/config/navbar-config.ts; Criação de uma constante em src/domain/config/navbar-config.ts

--- src/domain/config/navbar-config.ts
+++ src/domain/config/navbar-config.ts
@@ -17,6 +17,8 @@ export const RandomAlphanumeric = (length: number): string => {
 };
 
 export const RANDOM_CODE = Math.floor(Math.random() * 1000000000 + 1);
+export const RANDOM_CODE_10 = Math.floor(Math.random() * 10000000000 + 1);
+export const RANDOM_CODE_6 = Math.floor(Math.random() * 1000000 + 1);
 
 export const NavbarDatas = () => {
   const { t } = useTranslation();
