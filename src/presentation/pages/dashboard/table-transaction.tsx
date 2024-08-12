42e7e8adb2027ccd337dd7bf71225c6a7b3f6245
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-12 07:14:03 +0100

    fix: src/presentation/pages/dashboard/table-transaction.tsx. Criação de um botão em src/presentation/pages/dashboard/table-transaction.tsx; Modificação de código em src/presentation/pages/dashboard/table-transaction.tsx; Alteração de texto em src/presentation/pages/dashboard/table-transaction.tsx; Mudança de texto em um elemento HTML em src/presentation/pages/dashboard/table-transaction.tsx; Alteração em funções de manipulação de eventos em src/presentation/pages/dashboard/table-transaction.tsx; Alteração de conteúdo em array ou objeto em src/presentation/pages/dashboard/table-transaction.tsx

--- src/presentation/pages/dashboard/table-transaction.tsx
+++ src/presentation/pages/dashboard/table-transaction.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react';
 import { Transaction } from '../admin-pages/dashboard-transactions';
-
 
 interface AccordionTableProps {
     transactions: any[];
     approveTransaction: (transaction: Transaction) => void;
 }
+
 
 const AccordionTable: React.FC<AccordionTableProps> = ({ transactions, approveTransaction }) => {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -18,50 +19,51 @@ const AccordionTable: React.FC<AccordionTableProps> = ({ transactions, approveTr
         <div className="accordion">
             <button
                 onClick={toggleAccordion}
-                className="w-full text-left py-2 px-4 border border-gray-200 bg-gray-100 hover:bg-gray-200"
+                className="w-full px-4 py-2 text-left bg-gray-100 border border-gray-200 hover:bg-gray-200"
             >
                 {isExpanded ? 'Minimizar' : 'Expandir'} Tabela de atividades
             </button>
+
             {isExpanded && (
-                <div className="py4 border border-gray-200">
+                <div className="border border-gray-200 py4">
                     <table className="w-full bg-white border border-gray-200">
                         <thead>
                             <tr>
-                                <th className="py-2 text-sm text-start px-4 border-b">Student</th>
-                                <th className="py-2 text-sm text-start px-4 border-b">Email</th>
-                                <th className="py-2 text-sm text-start px-4 border-b">Curso</th>
-                                <th className="py-2 text-sm text-start px-4 border-b">Preço</th>
-                                <th className="py-2 text-sm text-start px-4 border-b">Modalidade</th>
-                                <th className="py-2 text-sm text-start px-4 border-b">Status</th>
-                                <th className="py-2 text-sm text-start px-4 border-b">Progresso</th>
-                                <th className="py-2 text-sm text-start px-4 border-b">Comprovante</th>
-                                <th className="py-2 text-sm text-start px-4 border-b">Ação</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Student</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Email</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Curso</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Preço</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Modalidade</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Status</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Progresso</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Comprovante</th>
+                                <th className="px-4 py-2 text-sm border-b text-start">Ação</th>
                             </tr>
                         </thead>
                         <tbody>
                             {transactions.map((transaction) => (
                                 <tr key={transaction.id}>
-                                    <td className="py-2 capitalize text-xs px-4 border-b">{transaction.studentName}</td>
-                                    <td className="py-2 text-xs px-4 border-b">{transaction.studentEmail}</td>
-                                    <td className="py-2 capitalize text-xs px-4 border-b">{transaction.course_name}</td>
-                                    <td className="py-2 capitalize text-xs px-4 border-b">{transaction.price} AOA</td>
-                                    <td className="py-2 capitalize text-xs px-4 border-b">{transaction.method}</td>
-                                    <td className="py-2 capitalize text-xs px-4 border-b">{transaction.statusPagamento}</td>
+                                    <td className="px-4 py-2 text-xs capitalize border-b">{transaction.studentName}</td>
+                                    <td className="px-4 py-2 text-xs border-b">{transaction.studentEmail}</td>
+                                    <td className="px-4 py-2 text-xs capitalize border-b">{transaction.course_name}</td>
+                                    <td className="px-4 py-2 text-xs capitalize border-b">{transaction.price} AOA</td>
+                                    <td className="px-4 py-2 text-xs capitalize border-b">{transaction.method}</td>
+                                    <td className="px-4 py-2 text-xs capitalize border-b">{transaction.statusPagamento}</td>
                                     <td className={`py-2 capitalize text-xs ${parseInt(transaction.progresso) === 100 && 'bg-green-200'} px-4 border-b`}>
                                         {transaction.progresso}%
                                     </td>
-                                    <td className="py-2 capitalize text-xs px-4 border-b">
+                                    <td className="px-4 py-2 text-xs capitalize border-b">
                                         {transaction.boletoFile && (
                                             <a href={transaction.boletoFile} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
                                                 Ver Comprovante
                                             </a>
                                         )}
                                     </td>
-                                    <td className="py-2 px-4 text-xs border-b">
+                                    <td className="px-4 py-2 text-xs border-b">
                                         {transaction.statusPagamento === 'pendente' && (
                                             <button
                                                 onClick={() => approveTransaction(transaction)}
-                                                className="bg-primary text-black px-2 py-1 rounded hover:bg-yellow-600"
+                                                className="px-2 py-1 text-black rounded bg-primary hover:bg-yellow-600"
                                             >
                                                 Aprovar
                                             </button>
