5342f6bc16e6e636683530e65dda278c8b4d6e22
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-12 08:04:13 +0100

    fix: src/domain/models/labs-model.ts. Modificação de código em src/domain/models/labs-model.ts

--- src/domain/models/labs-model.ts
+++ src/domain/models/labs-model.ts
@@ -2,7 +2,7 @@ import { ICtfsChallenge } from "@/interfaces/ctfs/ctfs-intrface";
 
 export interface IMLab {
   lab_id: number;
-  course_id: number;
+  course_id: string;
   lab_name: string;
   course_name: string;
   challenges: ICtfsChallenge[];
