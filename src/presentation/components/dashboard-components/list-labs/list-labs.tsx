17ebaad9ef0cfdb6a3cc334d81c0f575c289ee03
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-14 09:07:20 +0100

    fix: src/presentation/components/dashboard-components/list-labs/list-labs.tsx. Criação de uma imagem em src/presentation/components/dashboard-components/list-labs/list-labs.tsx; Modificação de código em src/presentation/components/dashboard-components/list-labs/list-labs.tsx; Alteração de texto em src/presentation/components/dashboard-components/list-labs/list-labs.tsx; Criação de uma constante em src/presentation/components/dashboard-components/list-labs/list-labs.tsx; Mudança de texto em um elemento HTML em src/presentation/components/dashboard-components/list-labs/list-labs.tsx; Alteração em funções de manipulação de eventos em src/presentation/components/dashboard-components/list-labs/list-labs.tsx; Alteração de conteúdo em array ou objeto em src/presentation/components/dashboard-components/list-labs/list-labs.tsx

--- src/presentation/components/dashboard-components/list-labs/list-labs.tsx
+++ src/presentation/components/dashboard-components/list-labs/list-labs.tsx
@@ -21,8 +21,10 @@ export const ListLabs: React.FC = () => {
       }
 
       try {
+
+        /*
         // Recupera os Treinamentos comprados pelo usuário
-        const userDocRef = db.collection('admins').doc(currentUser.uid);
+        const userDocRef = db.collection('alunos').doc(currentUser.uid);
         const userCoursesSnapshot = await userDocRef.collection('courses').where('statusPagamento', '==', 'aprovado').get();
         const purchasedCourses = userCoursesSnapshot.docs.map(doc => doc.id);
 
@@ -31,9 +33,9 @@ export const ListLabs: React.FC = () => {
           setLoading(false);
           return;
         }
-
+*/
         // Recupera labs que correspondem aos Treinamentos comprados
-        const labsSnapshot = await db.collection('labs').where('course_id', 'in', purchasedCourses).get();
+        const labsSnapshot = await db.collection('labs').get();
         const labsData = labsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as unknown as IMLab[];
         setLabs(labsData);
       } catch (error) {
@@ -57,23 +59,23 @@ export const ListLabs: React.FC = () => {
       {
         labs.length > 0 ?
 
-          <div className="grid 2xl:grid-cols-4 gap-8">
+          <div className="grid gap-8 2xl:grid-cols-4">
 
 
             {labs.map((lab) => (
-              <div onClick={() => window.location.href = `${ROUTE_VIRTUAL_LABS + '/' + lab.lab_id}`} key={lab.lab_id} className="dark:bg-slate-100/10 max-w-full hacker dark:border-white/40 relative items-center px-4 py-5 transition-all bg-white border rounded-lg shadow-md cursor-pointer     hacker-div  ">
+              <div onClick={() => window.location.href = `${ROUTE_VIRTUAL_LABS + '/' + lab.lab_id}`} key={lab.lab_id} className="relative items-center max-w-full px-4 py-5 transition-all bg-white border rounded-lg shadow-md cursor-pointer dark:bg-slate-100/10 hacker dark:border-white/40 hacker-div ">
                 <img src={icons.lab} className='absolute w-[7em] dark:opacity-[.2] opacity-[.03]' alt="" />
                 <div className="flex flex-wrap justify-between">
-                  <h2 className="mb-2 sm:text-4xl  text-4xl font-bold  dark:text-primary hacker text-yellow-700">{lab.lab_name}</h2>
-                  <h3 className='tracking-wider flex gap-2 dark:text-primary text-yellow-700'> <img src={svgs.square_svg} className='w-4 my-auto' alt="" /><span className="my-auto font-bold"> {lab.challenges.length} </span> </h3>
+                  <h2 className="mb-2 text-4xl font-bold text-yellow-700 sm:text-4xl dark:text-primary hacker">{lab.lab_name}</h2>
+                  <h3 className='flex gap-2 tracking-wider text-yellow-700 dark:text-primary'> <img src={svgs.square_svg} className='w-4 my-auto' alt="" /><span className="my-auto font-bold"> {lab.challenges.length} </span> </h3>
                 </div>
-                <div className="px-2  dark:bg-white/10 rounded-md dark:text-white py-1 text-start bg-gray-100/90 ">
-                  <b className="text-xs"><span className="font-bold dark:text-primary text-gray-700">Treinamento:</span> {lab.course_name}</b>
+                <div className="px-2 py-1 rounded-md dark:bg-white/10 dark:text-white text-start bg-gray-100/90 ">
+                  <b className="text-xs"><span className="font-bold text-gray-700 dark:text-primary">Treinamento:</span> {lab.course_name}</b>
                   <br />
                   {
                     /*
 
-                    <Link to={`/dashboard/labs/${lab.lab_id}`} className="text-yellow-700 dark:text-yellow-300 sm:tracking-wider hacker  underline">
+                    <Link to={`/dashboard/labs/${lab.lab_id}`} className="text-yellow-700 underline dark:text-yellow-300 sm:tracking-wider hacker">
                     Ver Desafios
                   </Link>
 
@@ -90,8 +92,8 @@ export const ListLabs: React.FC = () => {
             <FaFilter className='mx-auto text-gray-500 sm:text-9xl text-7xl' />
             <br />
             <br />
-            <h2 className="text-gray-500 text-xs sm:text-md hacker dark:text-white/70 lg:w-7/12 mx-auto">
-              Não há ainda laboratórios para sí, <a href={ROUTE_TRAININGS} className='underline text-yellow-600'>compre um treinamento</a> para começar com o seu primeiro <b className='hacker'>Laboratório</b>!
+            <h2 className="mx-auto text-xs text-gray-500 sm:text-md hacker dark:text-white/70 lg:w-7/12">
+              Não há ainda laboratórios para sí, <a href={ROUTE_TRAININGS} className='text-yellow-600 underline'>compre um treinamento</a> para começar com o seu primeiro <b className='hacker'>Laboratório</b>!
             </h2>
           </div>
       }
