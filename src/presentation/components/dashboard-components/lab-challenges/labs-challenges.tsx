16f0755b5e880b95d53362c5987a473166e7bdce
Joao Afonso Katombela <ja3328173@gmail.com>
2024-08-14 09:07:19 +0100

    fix: src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx. Criação de um botão em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx; Criação de uma imagem em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx; Estilização de CSS em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx; Modificação de código em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx; Alteração de texto em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx; Criação de uma constante em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx; Mudança de texto em um elemento HTML em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx; Alteração em funções de manipulação de eventos em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx; Alteração de conteúdo em array ou objeto em src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx

--- src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx
+++ src/presentation/components/dashboard-components/lab-challenges/labs-challenges.tsx
@@ -1,37 +1,28 @@
-// LabChallenges.tsx
-import { db } from '@/domain/config/firebase';
-import { IMLab } from '@/domain/models/labs-model';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { db } from '@/domain/config/firebase';
+import { IMLab } from '@/domain/models/labs-model';
 import { Navbar } from '../navbar/navbar';
-import { ROUTE_TRAININGS } from '@/utils/sidebar-utils';
-import { FaCartPlus, FaCheckCircle, FaRegTimesCircle } from 'react-icons/fa';
-import 'reactjs-popup/dist/index.css';
-import { useAuth } from '@/context/auth-context';
-import { StringUtils } from '@/utils/string-utils';
-import { ICtfsChallenge } from '@/interfaces/ctfs/ctfs-intrface';
-import { IHackerScore } from '@/domain/models/score-model';
-import { AlertUtils } from '@/utils/alert-utils';
-import { GetStatusColor } from '@/utils/get-status-color-utils';
-import firebase from 'firebase/compat/app';
-import { LoaderText } from '../loader-text/loader-text';
-import { Button, HakyModalDefault, Preloader } from '../..';
-import { FaCheck, FaLink } from 'react-icons/fa6';
 import { Sidebar } from '../sidebar/sidebar';
-
+import { useAuth } from '@/context/auth-context';
+import { ICtfsChallenge } from '@/interfaces/ctfs/ctfs-intrface';
+import { LabChallengeList } from '../../labs-components/labChallengeList';
+import { LabChallengeModal } from '../../labs-components/labChallengeMOdal';
+import { AlertUtils } from '@/utils/alert-utils';
+import firebase from 'firebase/compat/app';
+import { IHackerScore } from '@/domain/models/score-model';
 
 export const LabChallenges: React.FC = () => {
     const { labId } = useParams();
     const [lab, setLab] = useState<IMLab | null>(null);
-    const [flag, setFlag] = useState('');
-    const [loadingC, setLoadingC] = useState(false);
-    const [selectedLabChallenge, setselectedLabChallenge] = useState<ICtfsChallenge | null>(null);
+    const [selectedLabChallenge, setSelectedLabChallenge] = useState<ICtfsChallenge | null>(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [userCtfs, setUserCtfs] = useState<string[]>([]); // IDs dos CTFs resolvidos pelo usuário
+    const [userCtfs, setUserCtfs] = useState<string[]>([]);
+    const [loadingC, setLoadingC] = useState(false);
     const [msg, setMsg] = useState('');
-    const { currentUser, userSettings } = useAuth()
+    const { currentUser } = useAuth();
 
-    document.title = 'Laboratórios Virtuais | HakyOff Academy'
+    document.title = 'Admin | Laboratórios Virtuais';
 
     const [isOpen, setIsOpen] = useState(true); // Definir o estado isOpen aqui
 
@@ -39,33 +30,44 @@ export const LabChallenges: React.FC = () => {
         setIsOpen(!isOpen);
     };
 
-
     useEffect(() => {
         const fetchLab = async () => {
-            const querySnapshot = await db.collection('labs').where('lab_id', '==', labId ? labId : '').get();
+            const querySnapshot = await db.collection('labs').where('lab_id', '==', labId ?? '').get();
             if (!querySnapshot.empty) {
                 setLab(querySnapshot.docs[0].data() as IMLab);
             }
         };
-
         fetchLab();
     }, [labId]);
 
+    useEffect(() => {
+        const fetchUserCtfs = async () => {
+            if (currentUser) {
+                try {
+                    const userCtfsCollection = db.collection('admins').doc(currentUser.uid).collection('resolvedChallengesLab');
+                    const snapshot = await userCtfsCollection.get();
+                    const userCtfsData = snapshot.docs.map(doc => doc.id);
+                    setUserCtfs(userCtfsData);
+                } catch (error) {
+                    console.error('Erro ao buscar CTFs Lab do admin:', error);
+                }
+            }
+        };
+        fetchUserCtfs();
+    }, [currentUser]);
 
-
-    const openModal = (chal: ICtfsChallenge) => {
-        setselectedLabChallenge(chal);
+    const openModal = (challenge: ICtfsChallenge) => {
+        setSelectedLabChallenge(challenge);
         setIsModalOpen(true);
     };
 
     const closeModal = () => {
         setIsModalOpen(false);
-        setselectedLabChallenge(null);
-        setFlag('');
+        setSelectedLabChallenge(null);
+        setMsg('');
     };
 
-
-    const handleSubmitFlag = async () => {
+    const handleSubmitFlag = async (flag: string) => {
         if (!selectedLabChallenge || !currentUser) return;
         setLoadingC(true)
 
@@ -133,210 +135,33 @@ export const LabChallenges: React.FC = () => {
         }
     };
 
-
-
-
-    useEffect(() => {
-
-        const fetchUserCtfs = async () => {
-            if (currentUser) {
-                try {
-                    const userCtfsCollection = firebase.firestore().collection('admins').doc(currentUser.uid).collection('resolvedChallengesLab');
-                    const snapshot = await userCtfsCollection.get();
-                    const userCtfsData = snapshot.docs.map(doc => doc.id);
-                    setUserCtfs(userCtfsData);
-                } catch (error) {
-                    console.error('Erro ao buscar CTFs Lab do usuário:', error);
-                }
-            }
-        };
-
-
-        fetchUserCtfs();
-    }, [currentUser]);
-
-
-
-
-    if (!lab) {
-        return <Preloader />;
-    }
-
     return (
+        <>
 
-        <div className={`${userSettings.darkMode ? 'dark' : ''}`}>
-            <Navbar />
-            <Sidebar toggleSidebar={toggleSidebar} isOpen={isOpen} /> {/* Passar isOpen para Sidebar */}
-            <div className={`min-h-screen p-4 sm:pt-20 ${isOpen ? 'sm:ml-[10rem]' : 'sm:ml-[4rem]'} transition-all duration-300 bg-gray-100 dark:bg-gray-900`}>
-                <br />
-                <div className="flex dark:bg-slate-100/20 dark:text-white justify-between w-full px-5 py-6 bg-white rounded-lg shadow">
-                    <h2 className="sm:text-3xl text-2xl font-bold hacker"><b className='font-poppins'>LAB <span className="hacker">:</span> </b><span className="hacker">
-                        {lab.lab_name}</span></h2>
-                    <button className='click sm:hidden' onClick={() => window.location.href = ROUTE_TRAININGS}>
-                        <FaCartPlus className='my-auto dark:text-white text-black text-3xl sm:text-4xl' />
-                    </button>
-                </div>
-                <br />
+            <div className="">
+                <Navbar />
+                <Sidebar toggleSidebar={toggleSidebar} isOpen={isOpen} /> {/* Passar isOpen para Sidebar */}
 
-                {
-                    /*
-
-
-                    <div className="">
-                    <p className=''><b>Courso:</b> <span className="hacker ms-2"> {lab.course_name}</span></p>
-                    <br />
-                </div>
-
-
-                    */
-                }
-                <br />
-                <div className=" grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 ">
-                    {lab.challenges.map((challenge, index) => (
-                        <div onClick={() => userCtfs.includes(challenge.id) ? null : openModal(challenge)} title={`Clique para abrir ${challenge.title}`} key={index} className={`p-4 ${userCtfs.includes(challenge.id) ? '  ' : ' '} lab-bg  mb-4 hover:shadow-smmm shadow-inner hover:shadow-primaryy cursor-pointer  border border-white/20 relative rounded`}>
-
-                            <div className="flex mb-2 font-bold justify-end">
-                                {
-                                    userCtfs.includes(challenge.id)
-
-                                        ?
-                                        <FaCheck className=' text-xl my-auto me-2 ' />
-                                        :
-
-                                        null
-
-                                }
-                                {challenge.pts} Pontos
-                            </div>
-                            <hr className=' border-black' />
-
-                            {
-                                /*
-                                 <Popup trigger={<div className='flex gap-1   absolute right-6 top-6'>
-                                    <div className={`h-[4px] hidden   ${userCtfs.includes(challenge.id) ? 'bg-green-500' : 'bg-white/70'} w-full my-auto rounded-full `}></div>
-    
-    
-                                    <div className="hidden sm:block">
-                                        {
-                                            userCtfs.includes(challenge.id)
-    
-                                                ?
-                                                <FaCheckCircle className=' text-4xl text-green-500' />
-                                                :
-                                                <FaTimesCircle className=' text-4xl text-red-500/70' />
-    
-    
-                                        }
-                                    </div>
-    
-                                </div>
-                                } position={userCtfs.includes(challenge.id) ? "right center" : "left center"}>
-                                    {
-                                        userCtfs.includes(challenge.id) ?
-                                            <div className='text-sm p-1 hacker relative'>
-                                                <img src={extra.hack_face} className='absolute w-[5.8em] -top-3 right-0 opacity-[.15]' alt="" />
-                                                <div className="text-sm">
-                                                    <center>
-                                                        <FaCheckCircle className=' text-4xl mb-2 text-green-500' />
-    
-                                                    </center>
-                                                    <b>{StringUtils.getFirstWord(currentUser?.displayName ? currentUser.displayName : '')}</b>,  Parabéns , você pontuou neste desafio e foi acrescio no ranking, resolva os próximos desafios.</div></div>
-                                            :
-                                            <div className='text-sm p-1 hacker relative'>
-                                                <img src={extra.hack_face} className='absolute w-[5.8em] -top-3 right-0 opacity-[.15]' alt="" />
-                                                <b>{StringUtils.getFirstWord(currentUser?.displayName ? currentUser.displayName : '')}</b>,  Resolva este desafio para pontuar no Ranking e no seu perTreinamento de treinamento </div>
-                                    }
-                                </Popup>
-                                */
-                            }
-                            <br />
-                            <h4 className='sm:text-xl font-semibold mb-2'>{challenge.title}</h4>
-
-
-                            {
-
-                                /*
-                         <div className="gap-5 mb-2 flex flex-start justify-start  ">
-                                <p className={`my-auto border  px-2 border-white/50 sm:text-sm text-xs ${getCTFBackgroundColor(challenge.level)} text-primary`}> {challenge.level}</p>
-
-                                <p className=' my-auto  text-primary sm:text-md  '><span className="text-white">Pontos: </span>{challenge.pts}<span className=""> PTS</span></p>
-
-                            </div>
-                                 <button disabled={userCtfs.includes(challenge.id)} onClick={() => userCtfs.includes(challenge.id) ? null : openModal(challenge)} className={`  border px-2  border-white/50 text-sm   ${userCtfs.includes(challenge.id) ? 'bg-green-600 line-through text-white hacker font-semibold' : 'bg-primary hover:shadow-white  hover:shadow-md'} text-black rounded-sm  uppercase`}>   {userCtfs.includes(challenge.id) ? 'RESOLVIDO' : 'Resolver'}</button>
-    
-                                <p className='mt-4'>
-                                    {challenge.desc}
-    
-                                </p>
-    
-                                */
-                            }
-                        </div>
-                    ))}
+                <div className={`min-h-screen p-4 sm:pt-20 ${isOpen ? 'sm:ml-[10rem]' : 'sm:ml-[4rem]'} transition-all duration-300 bg-gray-100 dark:bg-gray-900`}>
+                    
+                    <div className="flex-grow p-6">
+                        <h1 className="text-2xl font-bold">Laboratório Virtual: {lab?.lab_name}</h1>
+                        <LabChallengeList
+                            challenges={lab?.challenges ?? []}
+                            userCtfs={userCtfs}
+                            onChallengeClick={openModal}
+                        />
+                    </div>
                 </div>
             </div>
-
-            {isModalOpen && selectedLabChallenge && (
-                <HakyModalDefault className='text-white sm:w-[40rem] h-[27rem] sm:p-10 py-10 px-3' setShow={closeModal} show={true} bgDefault={true} shadowDeault={true}>
-                    <h2 className="mb-1 text-xl font-bold">{selectedLabChallenge.title}</h2>
-                    <hr />
-                    <p className='mt-4'>
-                        ID: {selectedLabChallenge.id}
-                    </p>
-                    <div className="flex gap-3 my-3 text-sm hacker">
-                        <p className={`my-auto font-bold text-lg ${GetStatusColor(selectedLabChallenge.level)}`}> {selectedLabChallenge.pts} Pts</p>
-
-                    </div>
-                    <p>
-                        <span className='flex gap-4'>Link: <a target='__blank' href={selectedLabChallenge.link} className="text-yellow-600 underline flex gap-2">{selectedLabChallenge.link} <FaLink className='my-auto  ms-2' /></a></span>
-                        <div dangerouslySetInnerHTML={{ __html: selectedLabChallenge.desc }} />
-                        <br />
-                    </p>
-                    {
-                        msg === 'wrong' ?
-
-                            <>
-                                <div className="bg-red-400/30 text-sm  mb-2 text-white font-bold px-4 py-3 flex gap-4">
-                                    <FaRegTimesCircle className='my-auto ' />
-                                    <span className='my-auto'>Flag incorreta. Tente novamente.</span>
-                                </div>
-                            </> :
-
-                            msg === 'right' ?
-                                <>
-                                    <div className="bg-green-400/30 rounded-lg text-center text-white font-bold px-4 py-2 mb-2 flex flex-col gap-2">
-                                        <FaCheckCircle className='my-auto text-2xl mx-auto' />
-                                        <span className='my-auto text-sm'>Flag correta, acertou em cheio <span className="underline">{StringUtils.getFirstWord(currentUser?.displayName ? currentUser?.displayName : '')}</span>, resolva o seu próximo desafio ! </span>
-                                    </div>
-                                </>
-                                : null
-                    }
-                    <input
-                        type="text"
-                        value={flag}
-                        onChange={(e) => setFlag(e.target.value)}
-                        className='border-2 sm:text-md text-xs text-black outline-none focus-within:border-primary w-full py-2 px-2 rounded-md'
-                        placeholder='Insira a flag capturada'
-                    />
-                    <br />
-                    <div className="text-end mt-7">
-                        {
-                            loadingC ?
-                                <>
-                                    <LoaderText text='Verifcando sua flag.' />
-                                </> :
-
-                                <Button disabled={msg == 'right'} text='Submeter' color='primary' className='click sm:text-md text-xs text-black ms-auto' onClick={handleSubmitFlag} />
-                        }
-                    </div>
-                </HakyModalDefault>
-            )}
-
-
-
-        </div >
-
-
+            <LabChallengeModal
+                challenge={selectedLabChallenge}
+                isOpen={isModalOpen}
+                onClose={closeModal}
+                onSubmitFlag={handleSubmitFlag}
+                loading={loadingC}
+                msg={msg}
+            />
+        </>
     );
 };
-
